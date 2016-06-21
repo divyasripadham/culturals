98982c191d03b596ded4c9c3a006d72074abc2e1
divyasripadham <divya_sri16@yahoo.com>
2016-06-21 09:43:53 -0400

    Fixes #7

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ source 'https://rubygems.org'
    gem 'sqlite3'
  end
 
+ group :development, :test do
+   gem 'rspec-rails', '~> 3.0'
+ end
+
  # Use SCSS for stylesheets
  gem 'sass-rails', '~> 5.0'
  # Use Uglifier as compressor for JavaScript assets
@@ -22,3 +26,5 @@ source 'https://rubygems.org'
  gem 'jquery-rails'
  # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
  gem 'turbolinks'
+
+ gem 'bootstrap-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,23 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get 'videos/index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'videos/show'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'videos/new'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'videos/edit'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get 'events/index'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  get 'events/show'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get 'events/new'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  get 'events/edit'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  get 'welcome/index'
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get 'welcome/about'
+
+  root 'welcome#index'
 end
